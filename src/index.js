8085dc214eec46b68566cd991f7defda5a614cfa
Patrick Burls <pburls@gmail.com>
2019-11-24 20:17:52 +0000

    add react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.css';
 
 console.log('hello world!');
+
+const Index = () => (<div>
+    <h1>
+        Spectacular
+    </h1>
+</div>);
+
+ReactDOM.render(<Index />, document.getElementById("root"));
