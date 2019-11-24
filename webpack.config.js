c6811c414c903a49ad15e1f1095f17440b2722ef
Patrick Burls <pburls@gmail.com>
2019-11-24 19:45:52 +0000

    add webpack htm+css plugins

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+ const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+    filename: '[name].[contenthash:4].css',
+});
+
+ module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [htmlPlugin, miniCssExtractPlugin],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
