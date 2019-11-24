4de138736e70ae627e56b40ff4dde4385b897349
Patrick Burls <pburls@gmail.com>
2019-11-24 21:55:39 +0000

    add empty github service springboot app

---
+++ github-service/src/main/java/spectacular/github/service/GitHubServiceApplication.java
@@ -0,0 +1,13 @@
+package spectacular.github.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubServiceApplication.class, args);
+	}
+
+}
