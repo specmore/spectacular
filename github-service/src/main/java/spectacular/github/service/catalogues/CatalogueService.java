e746c9ab1efa45618a3e905613ca5a97fb591d90
Patrick Burls <pburls@gmail.com>
2019-11-26 23:14:28 +0000

    add spock tests

--- github-service/src/main/java/spectacular/github/service/catalogues/CatalogueService.java
+++ github-service/src/main/java/spectacular/github/service/catalogues/CatalogueService.java
@@ -1,7 +1,10 @@
 package spectacular.github.service.catalogues;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class CatalogueService {
     public List<CatalogueItem> getCatalogueItemsForAppConfig() {
         return List.of(new CatalogueItem("Test Catalogue 1", "pburls/specs-test"));
---
+++ github-service/src/test/groovy/spectacular/github/service/catalogues/CatalogueControllerTest.groovy
@@ -0,0 +1,31 @@
+package spectacular.github.service.catalogues
+
+import com.jayway.jsonpath.JsonPath
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+//Full integration tests
+@SpringBootTest
+@AutoConfigureMockMvc
+class CatalogueControllerTest extends Specification {
+    @Autowired
+    protected MockMvc mvc
+
+    def "when getCatalogues is performed then the response has status 200 and content is a catalogues response object"() {
+        expect: "Status is 200 and the response is a catalogues response object"
+        def mvcResult = mvc.perform(get("/api/catalogues"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath('$.catalogueItems.length()').value(1))
+                .andReturn()
+    }
+}
