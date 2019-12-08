2865a6e4fa7a96aef12fc0db0bac22a69597c4a6
Patrick Burls <pburls@gmail.com>
2019-12-08 21:06:26 +0000

    add AppInstallationContextProvider as a request scoped bean

--- github-service/src/main/java/spectacular/github/service/GitHubServiceApplication.java
+++ github-service/src/main/java/spectacular/github/service/GitHubServiceApplication.java
@@ -2,9 +2,19 @@ package spectacular.github.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+import spectacular.github.service.github.app.AppInstallationContextProvider;
 
 @SpringBootApplication
 public class GitHubServiceApplication {
+	@Bean
+	@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public AppInstallationContextProvider appInstallationContextProviderRequestScopedBean() {
+		return new AppInstallationContextProvider();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubServiceApplication.class, args);
