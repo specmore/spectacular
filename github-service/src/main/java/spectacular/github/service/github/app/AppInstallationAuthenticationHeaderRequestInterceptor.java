04b8f945cffad2f9fccceff3376c490164ded738
Patrick Burls <pburls@gmail.com>
2019-12-14 11:21:55 +0000

    fix spring bean registration

--- github-service/src/main/java/spectacular/github/service/github/app/AppInstallationAuthenticationHeaderRequestInterceptor.java
+++ github-service/src/main/java/spectacular/github/service/github/app/AppInstallationAuthenticationHeaderRequestInterceptor.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class AppInstallationAuthenticationHeaderRequestInterceptor implements ClientHttpRequestInterceptor {
     private final AppInstallationService appInstallationService;
     private final AppInstallationContextProvider appInstallationContextProvider;
