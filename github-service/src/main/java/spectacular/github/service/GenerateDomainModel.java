2532fcb2bd627b002361ca5148b9e66ee9c0c568
Patrick Burls <pburls@gmail.com>
2019-12-14 15:42:06 +0000

    add domain model diagram generation

---
+++ github-service/src/main/java/spectacular/github/service/GenerateDomainModel.java
@@ -0,0 +1,42 @@
+package spectacular.github.service;
+
+import ch.ifocusit.plantuml.classdiagram.ClassDiagramBuilder;
+import spectacular.github.service.catalogues.CatalogueController;
+import spectacular.github.service.catalogues.CatalogueService;
+import spectacular.github.service.catalogues.CataloguesResponse;
+import spectacular.github.service.config.instance.Catalogue;
+import spectacular.github.service.config.instance.InstanceConfig;
+import spectacular.github.service.config.instance.InstanceConfigManifest;
+import spectacular.github.service.config.instance.InstanceConfigService;
+import spectacular.github.service.github.RestApiClient;
+import spectacular.github.service.github.app.*;
+import spectacular.github.service.github.domain.Repository;
+
+public class GenerateDomainModel {
+    public static void main (String args[]) {
+        String diagram = new ClassDiagramBuilder()
+                .addClasse(
+                        CatalogueController.class,
+                        CatalogueService.class,
+                        CataloguesResponse.class,
+
+                        Catalogue.class,
+                        InstanceConfig.class,
+                        InstanceConfigManifest.class,
+                        InstanceConfigService.class,
+
+                        AccessTokenResult.class,
+                        AppApiClient.class,
+                        AppAuthenticationService.class,
+                        AppInstallationAccessTokenStore.class,
+                        AppInstallationAuthenticationHeaderRequestInterceptor.class,
+                        AppInstallationContextProvider.class,
+                        AppInstallationService.class,
+                        GitHubAppAuthenticationHeaderRequestInterceptor.class,
+                        Repository.class,
+                        RestApiClient.class
+                        )
+                .build();
+        System.out.println(diagram);
+    }
+}
