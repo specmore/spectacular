f5b143c094066cf5b28f016873eb6a9ba1039137
Patrick Burls <pburls@gmail.com>
2020-01-25 16:17:38 +0000

    disabled unneeded security filters for pure resource server

--- github-service/src/main/java/spectacular/github/service/SecurityConfiguration.java
+++ github-service/src/main/java/spectacular/github/service/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -37,6 +38,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         var jwtCookieToAuthorizationHeaderFilter = new JWTCookieToAuthorizationHeaderFilter(this.jwtCookieName);
 
         http
+            .csrf().disable()
+            .logout().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
             .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
