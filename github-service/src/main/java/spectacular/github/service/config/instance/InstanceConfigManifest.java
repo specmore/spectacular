9bc811289a1f706f9ad149f64280075f39c4dbec
Patrick Burls <pburls@gmail.com>
2020-01-28 23:12:15 +0000

    add name to instance config manifests

--- github-service/src/main/java/spectacular/github/service/config/instance/InstanceConfigManifest.java
+++ github-service/src/main/java/spectacular/github/service/config/instance/InstanceConfigManifest.java
@@ -5,13 +5,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class InstanceConfigManifest {
+    private final String name;
     private final List<Catalogue> catalogues;
 
-    public InstanceConfigManifest(@JsonProperty("catalogues") List<Catalogue> catalogues) {
+    public InstanceConfigManifest(@JsonProperty("name") String name, @JsonProperty("catalogues") List<Catalogue> catalogues) {
+        this.name = name;
         this.catalogues = catalogues;
     }
 
     public List<Catalogue> getCatalogues() {
         return catalogues;
+    }
+
+    public String getName() {
+        return name;
     }
 }
