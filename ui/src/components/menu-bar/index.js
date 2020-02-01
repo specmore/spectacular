51c2a64b3edb012ee375cc0c6da663cb23ab85b0
Patrick Burls <pburls@gmail.com>
2020-02-01 00:04:41 +0000

    add home link to nav bar

--- ui/src/components/menu-bar/index.js
+++ ui/src/components/menu-bar/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Menu, Icon } from 'semantic-ui-react';
-import UserMenuBarItem from '../user-menu-bar-item'
+import UserMenuBarItem from '../user-menu-bar-item';
+import { Link } from 'react-router-dom';
 
 const MenuBar = () => (
     <Menu fixed="top" inverted>
-        <Menu.Item header><Icon inverted name="sitemap"/>Spectacular</Menu.Item>
+        <Menu.Item header as={Link} to="/"><Icon inverted name="sitemap"/>Spectacular</Menu.Item>
         <Menu.Menu position='right'><UserMenuBarItem/></Menu.Menu>
     </Menu>
 );
