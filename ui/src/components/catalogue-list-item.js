05dc1a3a76a6ff10e938db0530db281f6a9a3aa5
Patrick Burls <pburls@gmail.com>
2020-02-02 17:10:43 +0000

    fix tabs in catalogue-list-item

--- ui/src/components/catalogue-list-item.js
+++ ui/src/components/catalogue-list-item.js
@@ -4,48 +4,48 @@ import ImagePlaceHolder from '../assets/images/image-placeholder.png';
 import { CatalogueContainerLinkButton } from "../routes";
 
 const CatalogueErrorItem = ({error, repository}) => (
-    <Item data-testid='catalogue-list-item-error-item'>
-      <Item.Content>
-        <Item.Description>
-          <Message icon negative>
-            <Icon name='warning sign' />
-            <Message.Content>
-              <Message.Header>An error occurred while parsing the catalogue manifest file in <a href={repository.htmlUrl} target='_blank'>{repository.nameWithOwner}</a></Message.Header>
-              {error}
-            </Message.Content>
-          </Message>
-        </Item.Description>
-      </Item.Content>
-    </Item>
-  );
+  <Item data-testid='catalogue-list-item-error-item'>
+    <Item.Content>
+      <Item.Description>
+        <Message icon negative>
+          <Icon name='warning sign' />
+          <Message.Content>
+            <Message.Header>An error occurred while parsing the catalogue manifest file in <a href={repository.htmlUrl} target='_blank'>{repository.nameWithOwner}</a></Message.Header>
+            {error}
+          </Message.Content>
+        </Message>
+      </Item.Description>
+    </Item.Content>
+  </Item>
+);
   
-  const CatalogueItemDetails = ({repository, catalogueManifest, selectButton}) => (
-    <Item data-testid='catalogue-list-item-details-item'>
-      <Item.Image size='tiny' src={ImagePlaceHolder} />
-      <Item.Content>
-        <Item.Header>{catalogueManifest.name}</Item.Header>
-        <Item.Description>
-          {catalogueManifest.description}
-        </Item.Description>
-        <Item.Extra>
-          {selectButton}
-          <Label as='a' href={repository.htmlUrl} target='_blank'>
-            <Icon name='github' />{repository.nameWithOwner}
-          </Label>
-          <Label color='teal'>
-            <Icon name='file alternate' />{catalogueManifest["spec-files"].length} specs
-          </Label>
-        </Item.Extra>
-      </Item.Content>
-    </Item>
-  );
+const CatalogueItemDetails = ({repository, catalogueManifest, selectButton}) => (
+  <Item data-testid='catalogue-list-item-details-item'>
+    <Item.Image size='tiny' src={ImagePlaceHolder} />
+    <Item.Content>
+      <Item.Header>{catalogueManifest.name}</Item.Header>
+      <Item.Description>
+        {catalogueManifest.description}
+      </Item.Description>
+      <Item.Extra>
+        {selectButton}
+        <Label as='a' href={repository.htmlUrl} target='_blank'>
+          <Icon name='github' />{repository.nameWithOwner}
+        </Label>
+        <Label color='teal'>
+          <Icon name='file alternate' />{catalogueManifest["spec-files"].length} specs
+        </Label>
+      </Item.Extra>
+    </Item.Content>
+  </Item>
+);
   
-  const CatalogueListItem = ({catalogue}) => {
-    const selectButton = (<CatalogueContainerLinkButton repository={catalogue.repository} />);
-  
-    if (catalogue.error) return (<CatalogueErrorItem error={catalogue.error} repository={catalogue.repository} />);
-  
-    return (<CatalogueItemDetails repository={catalogue.repository} catalogueManifest={catalogue.catalogueManifest} selectButton={selectButton} />);
-  };
+const CatalogueListItem = ({catalogue}) => {
+  const selectButton = (<CatalogueContainerLinkButton repository={catalogue.repository} />);
 
-  export default CatalogueListItem;
+  if (catalogue.error) return (<CatalogueErrorItem error={catalogue.error} repository={catalogue.repository} />);
+
+  return (<CatalogueItemDetails repository={catalogue.repository} catalogueManifest={catalogue.catalogueManifest} selectButton={selectButton} />);
+};
+
+export default CatalogueListItem;
