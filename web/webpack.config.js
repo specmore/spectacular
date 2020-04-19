1ea4f28277c07bcca7f4fdc52a1bca49d1278359
Patrick Burls <pburls@gmail.com>
2020-04-19 23:10:37 +0100

    integrate eslint into webpack build

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -27,6 +27,14 @@ module.exports = () => {
     module: {
       rules: [
         {
+          enforce: 'pre',
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'eslint-loader',
+          },
+        },
+        {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
