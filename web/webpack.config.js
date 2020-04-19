ca5118d5eff5a8c24e1545f6ee3993cfae0cdbe2
Patrick Burls <pburls@gmail.com>
2020-04-19 23:07:14 +0100

    fix eslint errors on webpack config file

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,72 +1,72 @@
-require('dotenv').config({ path: '../.env' })
+require('dotenv').config({ path: '../.env' });
 const path = require('path');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
+  template: './src/index.html',
+  filename: './index.html',
 });
 
 const miniCssExtractPlugin = new MiniCssExtractPlugin({
-    filename: '[name].[contenthash:4].css',
+  filename: '[name].[contenthash:4].css',
 });
 
 const definePlugin = new webpack.DefinePlugin({
-    VERSION: JSON.stringify(process.env.SEMVER),
-    SHORTSHA: JSON.stringify(process.env.SHORTSHA)
+  VERSION: JSON.stringify(process.env.SEMVER),
+  SHORTSHA: JSON.stringify(process.env.SHORTSHA),
 });
 
- module.exports = () => {
-    console.log('SPECTACULAR_GITHUB_APP_INSTALLATION_ID: ', process.env.SPECTACULAR_GITHUB_APP_INSTALLATION_ID);
-    console.log('VERSION: ', process.env.SEMVER);
-    console.log('SHORTSHA: ', process.env.SHORTSHA);
+module.exports = () => {
+  console.log('SPECTACULAR_GITHUB_APP_INSTALLATION_ID: ', process.env.SPECTACULAR_GITHUB_APP_INSTALLATION_ID);
+  console.log('VERSION: ', process.env.SEMVER);
+  console.log('SHORTSHA: ', process.env.SHORTSHA);
 
-    return {
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader"
-                    }
-                },
-                {
-                    test:/\.css$/,
-                    use:['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
-                },
-                {
-                    test: /\.(jpg|png)$/,
-                    use: {
-                    loader: 'url-loader',
-                    },
-                },
-            ]
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-        plugins: [definePlugin, htmlPlugin, miniCssExtractPlugin],
-        output: {
-            filename: '[name].[contenthash].js',
-            path: path.resolve(__dirname, 'dist'),
-            publicPath: '/'
+        {
+          test: /\.css$/,
+          use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
         },
-        devServer: {
-            historyApiFallback: {
-                disableDotRule: true
-            },
-            proxy: {
-                '/api': {
-                    target: 'http://localhost:5000', //actual api
-                    // target: 'http://localhost:5005', //wiremock
-                    headers: {
-                        'x-spec-installation-id' : process.env.SPECTACULAR_GITHUB_APP_INSTALLATION_ID
-                    }
-                },
-                '/login': {
-                    target: 'http://localhost:5001'
-                }
-            }
-        }
-    };
+        {
+          test: /\.(jpg|png)$/,
+          use: {
+            loader: 'url-loader',
+          },
+        },
+      ],
+    },
+    plugins: [definePlugin, htmlPlugin, miniCssExtractPlugin],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    devServer: {
+      historyApiFallback: {
+        disableDotRule: true,
+      },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000', // actual api
+          // target: 'http://localhost:5005', //wiremock
+          headers: {
+            'x-spec-installation-id': process.env.SPECTACULAR_GITHUB_APP_INSTALLATION_ID,
+          },
+        },
+        '/login': {
+          target: 'http://localhost:5001',
+        },
+      },
+    },
+  };
 };
