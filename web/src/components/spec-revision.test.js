aaac886e8d117e761550f91b7d6ddafbc768555c
Patrick Burls <pburls@gmail.com>
2020-05-16 14:55:08 +0100

    change spec-revision to use new generator

--- web/src/components/spec-revision.test.js
+++ web/src/components/spec-revision.test.js
@@ -3,22 +3,12 @@ import '@testing-library/jest-dom/extend-expect';
 import moment from 'moment';
 import SpecRevision from './spec-revision';
 import { renderWithRouter } from '../__tests__/test-utils';
-
-const validSpecItem = {
-  repository: {
-    owner: 'test-owner', name: 'specs-test', htmlUrl: 'https://github.com/test-owner/specs-test', nameWithOwner: 'test-owner/specs-test',
-  },
-  filePath: 'specs/example-template.yaml',
-  ref: 'master',
-  sha: 'e6f9f693f080018158d1dd0394c53ab354a8be42',
-  lastModified: '2020-02-18T22:33:51Z',
-  parseResult: { openApiSpec: { title: 'An empty API spec', version: '0.1.0', operations: [] }, errors: [] },
-};
+import Generator from '../__tests__/test-data-generator';
 
 describe('SpecRevision component', () => {
   test('shows latest agreed spec item with branch name and version', async () => {
     // given a spec item with branch name and version
-    const specItem = validSpecItem;
+    const specItem = Generator.SpecItem.generateSpecItem();
 
     // when component renders
     const { getByText } = renderWithRouter(<SpecRevision specItem={specItem} />);
@@ -32,7 +22,7 @@ describe('SpecRevision component', () => {
 
   test('shows last modified date relative to now', async () => {
     // given a spec item with a last modified date 5 minutes ago
-    const specItem = validSpecItem;
+    const specItem = Generator.SpecItem.generateSpecItem();
     specItem.lastModified = moment().subtract(5, 'minutes').toISOString();
 
     // when component renders
@@ -50,7 +40,7 @@ describe('SpecRevision component', () => {
 
   test('shows the branch color specified', async () => {
     // given a spec item with branch name and version
-    const specItem = validSpecItem;
+    const specItem = Generator.SpecItem.generateSpecItem();
 
     // and a branch colour of yellow
     const branchColor = 'yellow';
@@ -64,7 +54,7 @@ describe('SpecRevision component', () => {
 
   test('defaults to the color olive if no branch color is specified', async () => {
     // given a spec item with branch name and version
-    const specItem = validSpecItem;
+    const specItem = Generator.SpecItem.generateSpecItem();
 
     // when the component renders
     const { getByText } = renderWithRouter(<SpecRevision specItem={specItem} />);
