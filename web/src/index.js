aa3cc2df3593911b594bd25ed520868a1e03ef39
Patrick Burls <pburls@gmail.com>
2020-05-31 22:38:27 +0100

    add /api base path to restful-react fetching

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { RestfulProvider } from 'restful-react';
 import InstallationWelcome from './components/installation-welcome';
 import CatalogueContainer from './components/catalogue-container';
 import NotFound from './components/not-found';
@@ -10,25 +11,27 @@ import './index.css';
 import { CATALOGUE_LIST_ROUTE, CATALOGUE_CONTAINER_ROUTE, CATALOGUE_CONTAINER_WITH_SPEC_LOCATION_ROUTE } from './routes';
 
 const Index = () => (
-  <Router>
-    <MenuBar />
-    <div style={{ paddingTop: '4em' }}>
-      <div style={{ position: 'relative' }}>
-        <Switch>
-          <Route exact path={CATALOGUE_LIST_ROUTE}>
-            <InstallationWelcome />
-          </Route>
-          <Route exact path={[CATALOGUE_CONTAINER_ROUTE, CATALOGUE_CONTAINER_WITH_SPEC_LOCATION_ROUTE]}>
-            <CatalogueContainer />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+  <RestfulProvider base="/api">
+    <Router>
+      <MenuBar />
+      <div style={{ paddingTop: '4em' }}>
+        <div style={{ position: 'relative' }}>
+          <Switch>
+            <Route exact path={CATALOGUE_LIST_ROUTE}>
+              <InstallationWelcome />
+            </Route>
+            <Route exact path={[CATALOGUE_CONTAINER_ROUTE, CATALOGUE_CONTAINER_WITH_SPEC_LOCATION_ROUTE]}>
+              <CatalogueContainer />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
-    <FooterBar />
-  </Router>
+      <FooterBar />
+    </Router>
+  </RestfulProvider>
 );
 
 ReactDOM.render(<Index />, document.getElementById('root'));
