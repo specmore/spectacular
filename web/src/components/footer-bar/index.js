a65de1be5ce5fc48415f26857ca329c0a500b844
Patrick Burls <pburls@gmail.com>
2020-06-24 21:17:19 +0100

    change project github location to specmore/spectacular

--- web/src/components/footer-bar/index.js
+++ web/src/components/footer-bar/index.js
@@ -6,7 +6,7 @@ const FooterBar = () => (
     <p>
       Contribute to the
       {' '}
-      <a href="https://github.com/pburls/spectacular" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/specmore/spectacular" target="_blank" rel="noopener noreferrer">
         <Icon name="github" />
         GitHub project
       </a>
