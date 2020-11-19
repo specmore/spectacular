749961372d351f5946d074cea5db6f000485ddf2
Patrick Burls <pburls@gmail.com>
2020-11-19 22:24:24 +0000

    change evolution to simple item group

--- web/src/components/spec-evolution.tsx
+++ web/src/components/spec-evolution.tsx
@@ -1,84 +1,43 @@
 import React, { FunctionComponent } from 'react';
 import {
-  Header,
+  Header, Item,
 } from 'semantic-ui-react';
-import { Gitgraph, templateExtend, TemplateName } from '@gitgraph/react';
-import { SpecLog } from '../backend-api-client';
+import { SpecItem, SpecLog } from '../backend-api-client';
 import { CloseSpecEvolutionButton } from '../routes';
 
 const MASTER_BRANCH_COLOR = '#5E81AC';
 const PR_BRANCH_COLOR = '#8FBCBB';
 const MESSAGE_COLOR = '#2E3440';
 
+interface SpecLogItemProps {
+  specItem: SpecItem;
+}
+
+const SpecLogItem: FunctionComponent<SpecLogItemProps> = ({ specItem }) => (
+  <Item>
+    <Item.Content>
+      <div>{specItem.ref}</div>
+    </Item.Content>
+  </Item>
+);
+
 interface SpecLogProps {
   specLog: SpecLog;
 }
 
-const SpecEvolutionContainer: FunctionComponent<SpecLogProps> = ({ specLog }) => {
-  // const gitgraphOption = { mode: Mode.Compact };
-  const template = templateExtend(TemplateName.Metro, {
-    commit: {
-      message: {
-        displayAuthor: false,
-        displayHash: false,
-        color: MESSAGE_COLOR,
-      },
-    },
-  });
-  const gitgraphOption = {
-    author: ' ',
-    template,
-  };
-
-  return (
-    <div data-testid="spec-evolution-container">
-      <CloseSpecEvolutionButton />
-      <Header as="h3">Spec Evolution</Header>
-      <Gitgraph options={gitgraphOption}>
-        {(gitgraph) => {
-          const master = gitgraph.branch({
-            name: 'master',
-            style: {
-              color: MASTER_BRANCH_COLOR,
-            },
-            commitDefaultOptions: {
-              style: {
-                color: MASTER_BRANCH_COLOR,
-                dot: {
-                  color: MASTER_BRANCH_COLOR,
-                },
-              },
-            },
-          });
-          const latestAgreedCommit = master.commit({
-            subject: 'Latest agreed version',
-            tag: specLog.latestAgreed.parseResult.openApiSpec.version,
-          });
-
-          console.log('latestAgreedCommit', latestAgreedCommit);
-
-          specLog.proposedChanges.forEach((proposedChange) => {
-            const branchName = `PR #${proposedChange.pullRequest.number}`;
-            const prBranch = latestAgreedCommit.branch({
-              name: branchName,
-              style: {
-                color: PR_BRANCH_COLOR,
-              },
-              commitDefaultOptions: {
-                style: {
-                  color: PR_BRANCH_COLOR,
-                  dot: {
-                    color: PR_BRANCH_COLOR,
-                  },
-                },
-              },
-            });
-            prBranch.commit(proposedChange.pullRequest.title);
-          });
-        }}
-      </Gitgraph>
-    </div>
-  );
-};
+const SpecEvolutionContainer: FunctionComponent<SpecLogProps> = ({ specLog }) => (
+  <div data-testid="spec-evolution-container">
+    <CloseSpecEvolutionButton />
+    <Header as="h3">Spec Evolution</Header>
+    <Item.Group divided>
+      {
+        specLog.proposedChanges.map((proposedChange) => (
+          <SpecLogItem key={proposedChange.specItem.id} specItem={proposedChange.specItem} />
+        ))
+      }
+      <SpecLogItem specItem={specLog.latestAgreed} />
+    </Item.Group>
+  </div>
+);
 
 export default SpecEvolutionContainer;
