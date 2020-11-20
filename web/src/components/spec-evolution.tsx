beb91484ae6ef1dd65105b9aa86e7dbc73518918
Patrick Burls <pburls@gmail.com>
2020-11-20 22:46:31 +0000

    add new details-container and centre flex grow div

--- web/src/components/spec-evolution.tsx
+++ web/src/components/spec-evolution.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import './spec-evolution.less';
 import {
-  Header, Item, Label,
+  Header, Label,
 } from 'semantic-ui-react';
 import { ChangeProposal, SpecItem, SpecLog } from '../backend-api-client';
 import { CloseSpecEvolutionButton, ViewSpecLinkButton } from '../routes';
@@ -17,23 +17,28 @@ interface ChangeProposalProps {
 }
 
 const ChangeProposalItem: FunctionComponent<ChangeProposalProps> = ({ proposedChange, interfaceName }) => (
-  <div className="log-item-container">
-    <div className="log-line-container" />
-    <Label color="green">
-      PR #
-      {proposedChange.pullRequest.number}
-    </Label>
-    <div>{proposedChange.pullRequest.title}</div>
-    <ViewSpecLinkButton refName={proposedChange.specItem.ref} interfaceName={interfaceName} />
+  <div className="log-entry-container">
+    <div className="line-container" />
+    <div className="details-container">
+      <Label color="green">
+        PR #
+        {proposedChange.pullRequest.number}
+      </Label>
+      <div className="centre">{proposedChange.pullRequest.title}</div>
+      <ViewSpecLinkButton refName={proposedChange.specItem.ref} interfaceName={interfaceName} />
+    </div>
   </div>
 );
 
 const LatestAgreedLogItem: FunctionComponent<SpecLogItemProps> = ({ specItem, interfaceName }) => (
-  <div className="log-item-container">
-    <div className="log-line-container" />
-    <Label color="blue">{specItem.ref}</Label>
-    <Label color="blue" tag>{specItem.parseResult.openApiSpec.version}</Label>
-    <ViewSpecLinkButton refName={specItem.ref} interfaceName={interfaceName} />
+  <div className="log-entry-container">
+    <div className="line-container" />
+    <div className="details-container">
+      <Label color="blue">{specItem.ref}</Label>
+      <Label color="blue" tag>{specItem.parseResult.openApiSpec.version}</Label>
+      <div className="centre" />
+      <ViewSpecLinkButton refName={specItem.ref} interfaceName={interfaceName} />
+    </div>
   </div>
 );
 
