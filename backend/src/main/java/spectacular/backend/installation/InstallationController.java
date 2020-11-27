ca221e2bb385a5a44b6e641f012f3e0d09d4d6d5
Patrick Burls <pburls@gmail.com>
2020-11-27 19:08:19 +0000

    update installation controller to use new generated interface from openapi spec

--- backend/src/main/java/spectacular/backend/installation/InstallationController.java
+++ backend/src/main/java/spectacular/backend/installation/InstallationController.java
@@ -1,10 +1,13 @@
 package spectacular.backend.installation;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spectacular.backend.api.InstallationApi;
+import spectacular.backend.api.model.Installation;
 
 @RestController
-public class InstallationController {
+public class InstallationController implements InstallationApi {
   private final InstallationService installationService;
 
   public InstallationController(InstallationService installationService) {
@@ -15,5 +18,10 @@ public class InstallationController {
   public InstallationResponse getCurrentInstallation() {
     var installation = this.installationService.getCurrentInstallation();
     return new InstallationResponse(installation);
+  }
+
+  @Override
+  public ResponseEntity<Installation> getInstallation() {
+    return null;
   }
 }
