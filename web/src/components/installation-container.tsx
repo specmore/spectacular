fe49fcd67c22419a74cedd0aefb62e487f2c56e3
Patrick Burls <pburls@gmail.com>
2021-01-17 20:48:26 +0000

    change installation container to use restful-react getInstallation fetcher

--- web/src/components/installation-container.tsx
+++ web/src/components/installation-container.tsx
@@ -3,64 +3,58 @@ import {
   Dimmer, Loader, Message, Container,
 } from 'semantic-ui-react';
 import { Switch, Route } from 'react-router-dom';
-import { fetchInstallation } from '../api-client';
 import EmptyWelcomeItemImage from '../assets/images/empty-catalogue-item.png';
 import CatalogueContainer from './catalogue-container';
 import CatalogueList from './catalogue-list';
 import NotFound from './not-found';
 import { CATALOGUE_LIST_ROUTE, CATALOGUE_CONTAINER_ROUTE, CATALOGUE_CONTAINER_WITH_SPEC_LOCATION_ROUTE } from '../routes';
 import InterfaceContainer from './interface-container';
+import { useGetInstallation } from '../backend-api-client';
+
+const InstallationContainerLoading: FunctionComponent = () => (
+  <Container text>
+    <Dimmer inverted active>
+      <Loader content="Loading" />
+    </Dimmer>
+    <img src={EmptyWelcomeItemImage} alt="placeholder" />
+  </Container>
+);
+
+interface InstallationContainerErrorProps {
+  errorMessage: string;
+}
+
+const InstallationContainerError: FunctionComponent<InstallationContainerErrorProps> = ({ errorMessage }) => (
+  <Container text style={{ paddingTop: '4em' }}>
+    <Message negative>
+      <Message.Header>{errorMessage}</Message.Header>
+    </Message>
+  </Container>
+);
 
 const InstallationContainer: FunctionComponent = () => {
-  const [installation, setInstallation] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const getInstallation = useGetInstallation({});
+  const { data: getInstallationResult, loading, error } = getInstallation;
 
-  const fetchInstallationData = async () => {
-    try {
-      const installationData = await fetchInstallation();
-      setInstallation(installationData);
-    } catch (error) {
-      // console.error(error);
-      setErrorMessage('An error occurred while fetching installation details.');
-    }
-  };
-
-  useEffect(() => {
-    fetchInstallationData();
-  }, []);
-
-  if (!installation && !errorMessage) {
-    return (
-      <Container text>
-        <Dimmer inverted active>
-          <Loader content="Loading" />
-        </Dimmer>
-        <img src={EmptyWelcomeItemImage} alt="placeholder" />
-      </Container>
-    );
+  if (loading) {
+    return (<InstallationContainerLoading />);
   }
 
-  if (errorMessage) {
-    return (
-      <Container text style={{ paddingTop: '4em' }}>
-        <Message negative>
-          <Message.Header>{errorMessage}</Message.Header>
-        </Message>
-      </Container>
-    );
+  if (error) {
+    return (<InstallationContainerError errorMessage={error.message} />);
   }
 
   return (
     <>
       <Switch>
         <Route exact path={CATALOGUE_LIST_ROUTE}>
-          <CatalogueList org={installation.owner} />
+          <CatalogueList org={getInstallationResult.owner} />
         </Route>
         <Route exact path={[CATALOGUE_CONTAINER_ROUTE]}>
-          <CatalogueContainer org={installation.owner} />
+          <CatalogueContainer org={getInstallationResult.owner} />
         </Route>
         <Route exact path={[CATALOGUE_CONTAINER_WITH_SPEC_LOCATION_ROUTE]}>
-          <InterfaceContainer org={installation.owner} />
+          <InterfaceContainer org={getInstallationResult.owner} />
         </Route>
         <Route path="*">
           <NotFound />
