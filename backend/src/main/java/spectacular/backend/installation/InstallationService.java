f0e1ee2a1a0c41ec24c4a4f51d61b6a18a6322fc
Patrick Burls <pburls@gmail.com>
2022-09-15 22:13:58 +0100

    remove unused getCurrentInstallation function from InstallationService

--- backend/src/main/java/spectacular/backend/installation/InstallationService.java
+++ backend/src/main/java/spectacular/backend/installation/InstallationService.java
@@ -11,26 +11,18 @@ import spectacular.backend.github.app.AppInstallationContextProvider;
 @Service
 public class InstallationService {
   private final AppApiClient appApiClient;
-  private final AppInstallationContextProvider appInstallationContextProvider;
   private final InstallationMapper installationMapper;
 
 
   /**
    * A service for retrieving information about an installation of Spectacular on a source control platform.
    * @param appApiClient an API client for the source control platform
-   * @param appInstallationContextProvider a service that provides additional contexts about the installation
    * @param installationMapper a mapper to translate the API responses into a generic installation object
    */
-  public InstallationService(AppApiClient appApiClient, AppInstallationContextProvider appInstallationContextProvider,
+  public InstallationService(AppApiClient appApiClient,
                              InstallationMapper installationMapper) {
     this.appApiClient = appApiClient;
-    this.appInstallationContextProvider = appInstallationContextProvider;
     this.installationMapper = installationMapper;
-  }
-
-  public Installation getCurrentInstallation() {
-    var gitHubInstallation = appApiClient.getAppInstallation(appInstallationContextProvider.getInstallationId());
-    return installationMapper.mapInstallation(gitHubInstallation);
   }
 
   public Installation getInstallation(Integer installationId) {
