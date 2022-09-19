0f061ce633aa304f1b75c69c44ed1b8368cb8c58
Patrick Burls <pburls@gmail.com>
2022-09-19 01:14:13 +0100

    finished removing axios

--- web/src/components/menu-bar/user-menu-bar-item.test.js
+++ web/src/components/menu-bar/user-menu-bar-item.test.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import axiosMock from 'axios';
 import UserMenuBarItem from './user-menu-bar-item';
+import { useGetUserDetails as useGetUserDetailsMock } from '../../backend-api-client';
 
-jest.mock('axios');
+jest.mock('../../backend-api-client');
 
 describe('UserMenuBarItem component', () => {
   test('successful fetch displays user menu', async () => {
-    // given a mocked successful user response
-    const userResponse = {
-      data: {
-        sub: 'pburls',
-        name: 'Patrick Burls',
-        picture: 'https://avatars2.githubusercontent.com/u/11502284?v=4',
-        origin: 'github',
-        exp: 1580050167,
-      },
+    // given a successful user details response
+    const userDetails = {
+      username: 'pburls',
+      fullName: 'Patrick Burls',
+      profileImageUrl: 'https://avatars2.githubusercontent.com/u/11502284?v=4',
     };
 
-    axiosMock.get.mockResolvedValueOnce(userResponse);
+    // and a mocked successful catalogue response
+    const userDetailsResponse = {
+      data: userDetails,
+    };
+
+    useGetUserDetailsMock.mockReturnValueOnce(userDetailsResponse);
 
     // when user menu bar item component renders
     const { findByText } = render(<UserMenuBarItem />);
+
+    // then it fetched the user details
+    expect(useGetUserDetailsMock).toHaveBeenCalledTimes(1);
 
     // then it contains an item for each catalogue in the response
     expect(await findByText('Patrick Burls')).toBeInTheDocument();
@@ -32,23 +36,12 @@ describe('UserMenuBarItem component', () => {
     expect(await findByText('Sign out')).toBeInTheDocument();
   });
 
-  // test("unsuccessful fetch displays error message", async () => {
-  //   // given a mocked error thrown
-  //   axiosMock.get.mockImplementation(() => {
-  //       throw new Error("test error");
-  //   });
-
-  //   // when user menu bar item component renders
-  //   const { findByText } = render(<UserMenuBarItem />);
-
-  //   // then it contains an error message
-  //   expect(await findByText("An error occurred while fetching user info.")).toBeInTheDocument();
-  // });
-
   test('loader is shown before fetch result', async () => {
-    // given a mocked user response that is not yet resolved
-    const responsePromise = new Promise(() => {});
-    axiosMock.get.mockImplementation(() => responsePromise);
+    // given a mocked catalogues response that is not yet resolved
+    const getCatalogueResponse = {
+      loading: true,
+    };
+    useGetUserDetailsMock.mockReturnValueOnce(getCatalogueResponse);
 
     // when user menu bar item component renders
     const { queryByText } = render(<UserMenuBarItem />);
